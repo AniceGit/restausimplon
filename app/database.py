c3bf506401f9022fb7d1cddbd47bbfbc4176c4e1
stephane <Vous@exemple.com>
2025-08-06 11:41:22 +0200

    update branche

--- app/database.py
+++ app/database.py
@@ -1,6 +1,5 @@
 from sqlmodel import SQLModel, create_engine, Session
-from app.crud.produit import creer_produit
-from app.schemas.produit import ProduitCreate
+
 
 
 DATABASE_URL = "sqlite:///./restausimplon.db"
@@ -12,7 +11,3 @@ def create_db_and_tables():
 def get_session():
     with Session(engine) as session:
         yield session
-
-
-
-
--- app/routers/produit.py
+++ app/routers/produit.py
@@ -1,5 +1,5 @@
 from sqlmodel import SQLModel, create_engine, Session
-from fastapi import APIRouter, Depends, HTTPException, Query
+from fastapi import APIRouter, Depends, Query
 from typing import List
 from app.crud.produit import creer_produit, get_all_produits, suppression_produit, modification_produit, rechercher_produits
 from app.database import get_session
@@ -15,22 +15,17 @@ def read_produits(session: Session = Depends(get_session)):
 
 @router.post("/", response_model=ProduitRead)
 def create_produit(produit: ProduitCreate, session: Session = Depends(get_session)):
-    # Utilisez la fonction creer_produit pour ajouter le produit à la base de données
     return creer_produit(produit, session)
 
 @router.delete("/{produit_id}", response_model=ProduitRead)
 def delete_produit(produit_id: int, session: Session = Depends(get_session)):
     produit = suppression_produit(produit_id, session)
-    if produit is None:
-        raise HTTPException(status_code=404, detail="Produit non trouvé")
     return produit
 
 @router.put("/{produit_id}", response_model=ProduitRead)
 def update_produit(produit_id: int, produit: ProduitUpdate, session: Session = Depends(get_session)):
     produit_data = produit.model_dump(exclude_unset=True)
     updated_produit = modification_produit(produit_id, produit_data, session)
-    if updated_produit is None:
-        raise HTTPException(status_code=404, detail="Produit non trouvé")
     return updated_produit
 
 @router.get("/search", response_model=List[ProduitRead])
