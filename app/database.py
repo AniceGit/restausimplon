8888a0f0e2fbfd16821450e515b18cd1de65b1e4
stephane <Vous@exemple.com>
2025-08-19 17:03:00 +0200

    update dockercompose user ok

--- app/database.py
+++ app/database.py
@@ -1,15 +1,15 @@
-# from sqlmodel import SQLModel, create_engine, Session
-# from app.core.config import settings  # import de la config
+from sqlmodel import SQLModel, create_engine, Session
+from app.core.config import settings  # import de la config
 
-# DATABASE_URL = settings.DATABASE_URL
-# engine = create_engine(DATABASE_URL, echo=True)
+DATABASE_URL = settings.DATABASE_URL
+engine = create_engine(DATABASE_URL, echo=True)
 
-# def create_db_and_tables():
-#     SQLModel.metadata.create_all(engine)
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
 
-# def get_session():
-#     with Session(engine) as session:
-#         yield session
+def get_session():
+    with Session(engine) as session:
+        yield session
 
 
 from sqlalchemy import create_engine
@@ -17,15 +17,15 @@ from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 import os
 
-# Utilise SQLite en mémoire pour les tests
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///:memory:")
+# # Utilise SQLite en mémoire pour les tests
+# DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///:memory:")
 
-engine = create_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# engine = create_engine(DATABASE_URL, echo=True)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-def get_session():
-    session = SessionLocal()
-    try:
-        yield session
-    finally:
-        session.close()
+# def get_session():
+#     session = SessionLocal()
+#     try:
+#         yield session
+#     finally:
+#         session.close()
